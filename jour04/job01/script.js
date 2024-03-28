decdb838515c1cc68fec39ddd9f693c122b8fedd
unknown <burton.tyll@laplateforme.io>
2024-03-28 12:01:03 +0100

    job01 fini

--- jour04/job01/script.js
+++ jour04/job01/script.js
@@ -1,6 +1,9 @@
 $("#button").click(function(){
 	fetch("expression.txt")
 	.then(response => response.text())
-	.then(data => console.log(data))
+	.then(data => {
+		console.log(data);
+		$("#phrase").html("<p>" + data + "</p>");
+	})
 	.catch(error => console.error("Une erreur est survenue", error));
 })
