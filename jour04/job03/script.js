0b5a018b10de025d2669756deb958c9feebcd870
burton-tyll <160846164+burton-tyll@users.noreply.github.com>
2024-03-29 12:50:39 +0100

    jour05

--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,12 +1,86 @@
-const select = $('#select');
-const resultSection = $('#resultat');
+// Sélection des éléments HTML avec jQuery et stockage dans des variables
+const select = $("#select");
+var pokémons;
+let valeurid;
+let valeurnom;
+let valeurtype;
 
-fetch('pokemon.json')
+// Utilisation de l'API Fetch pour récupérer les données du fichier JSON
+fetch("pokemon.json")
 	.then(response => response.json())
-	.then(data => {
-		const typesSet = new Set();
-		typeSet.add(data);
-		console.log(typesSet);
+	.then (data => {
+		pokémons = data;
+		// Création d'un ensemble (Set) pour stocker les types de Pokémon uniques
+		types = new Set()
+		// Parcours de chaque objet Pokémon dans les données
+		data.forEach(pokemon => {
+			// Parcours de chaque type du Pokémon
+			pokemon.type.forEach(type => {
+				// Ajout du type à l'ensemble
+				types.add(type)
+			})
+		})
+		// Parcours de chaque type unique dans l'ensemble
+		types.forEach(type => {
+			// Création d'une nouvelle option pour l'élément de sélection
+			$('<option>', {value: type, text: type}).appendTo(select)// Valeur de l'option et Texte affiché de l'option
+		})
+		$("#button").click(test)
 	})
+	// Gestion des erreurs
+	.catch (error => console.error("Erreur dans la récupération des données: ", error))
+
+
+	//--------------FONCTIONS-------------------
+
+	
+
+	function recupEntrée(){
+		valeurid = $('input[name="id"]').val();
+		valeurnom = $('input[name="nom"]').val();
+		valeurtype = $('select').val();
+		valeurs = {
+			id: valeurid, 
+			name: valeurnom, 
+			type: valeurtype
+		}
+	}
+
+	function extractPokemons(){
+		pokémons.forEach(pokemon => {
+			console.log(pokemon)
+		})
+	}
+
+	function comparer(){
+		pokémons.forEach(pokemon => {
+			if (pokemon.id === parseInt(valeurid)){
+				return true
+			}
+			else{
+				return false;
+			}
+		})
+	}
+
+	function test(){
+		recupEntrée()
+		comparer()
+		console.log(valeurs)
+	}
+
+
+
+		// if (pokémons.some(pokemon => pokemon.id === parseInt(valeurid))){
+		// 	console.log("yes")
+		// }
+
+
+
+
+
+
+
+
 
 
---
+++ jour05/job01/inscription.js
@@ -0,0 +1,38 @@
+const user = 
+
+$('button').click(checkForm)
+
+
+/*Fonctions*/
+
+function checkForm(){
+	recupValeurs()
+	verifPrenom(user)
+}
+
+function recupValeurs(){
+
+	let nom = $('input[name=nom]').val()
+	let prenom = $("input[name=prenom").val()
+	let email = $("input[name=email]").val()
+	let mdp = $("input[name=mdp]").val()
+	let adresse = $('input[name=adresse]').val()
+	let cp = $('input[name=codepostal]').val()
+	
+	//Stockage des valeurs dans un objet valeurs
+	user.nom = nom;
+	user.prenom = prenom;
+	user.email = email;
+	user.mdp = mdp;
+	user.adresse = adresse;
+	user.cp = cp;
+}
+
+function verifPrenom(user){
+	if (user.prenom.length > 3){
+		console.log(user.prenom)
+	}
+	else{
+		console.log("nope")
+	}
+}
